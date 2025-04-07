2c35d0cb53a57a9d3664688fe62645d1f9aab520
Juan Daniel <juan.gdanilo@gmail.com>
2025-04-07 18:08:33 -0500

    Created test with Pytest

--- globoticket/api.py
+++ globoticket/api.py
@@ -8,10 +8,10 @@ from pathlib import Path
 from typing import Annotated, Generator
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 from starlette.staticfiles import StaticFiles
 
+from globoticket.crud import get_all_dbevents, get_dbevent
 from globoticket.database import SessionLocal
 from globoticket.models import DBEvent
 from globoticket.schemas import Event
@@ -32,16 +32,15 @@ def get_session() -> Generator[Session, None, None]:
 @app.get("/event/{id}", response_model=Event)
 def get_event(id: int, db: Annotated[Session, Depends(get_session)]) -> DBEvent:
     """Retrieve a single event by id. Returns status 404 if event is not found."""
-    event = db.get(DBEvent, id)
+    event = get_dbevent(id, db)
     if event is None:
-        raise HTTPException(status_code=404, detail="No product with id {id}")
+        raise HTTPException(status_code=404, detail=f"No product with id {id}")
     return event
 
 
 @app.get("/event/", response_model=list[Event])
 def get_all_events(db: Annotated[Session, Depends(get_session)]) -> list[DBEvent]:
-    """Retrieve all events."""
-    return db.execute(select(DBEvent)).scalars()
+    return get_all_dbevents(db)
 
 
 app.mount("/", StaticFiles(directory=PROJECT_ROOT / "static", html=True))
